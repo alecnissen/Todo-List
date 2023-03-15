2a17cf215ab47e865086347646d01c7bb4b6e23e
alecnissen <alecnissen@yahoo.com>
2023-03-14 22:04:30 -0400

    added sass loader

--- src/index.js
+++ src/index.js
@@ -1,22 +1,38 @@
-document.getElementById("add-project-btn").addEventListener("click", function(){
-    
-    const inputField = document.createElement("input"); 
+
+
+// make a factory function for projects
+
+// make factory function for to-do/subtasks within the project.
+
+const projectFactory = (title, description, dueDate, priority) => {
+  return {
+    title,
+    description,
+    dueDate,
+    priority,
+  };
+}; 
+
+let person1
+
+ document.getElementById("add-project-btn").addEventListener("click", function(){
+    const inputField = document.createElement("input");
 
     inputField.classList.add("input-field-text");
 
-    const btnContainer = document.createElement("container"); 
+    const btnContainer = document.createElement("container");
 
     btnContainer.classList.add("btn-container");
 
-    const addBtn = document.createElement("button"); 
+    const addBtn = document.createElement("button");
 
-    addBtn.textContent = "Add Book";
+    addBtn.textContent = "Add";
 
-    const cancelBtn = document.createElement("button"); 
+    const cancelBtn = document.createElement("button");
 
-    cancelBtn.textContent = "Cancel"; 
+    cancelBtn.textContent = "Cancel";
 
-    addBtn.classList.add("add-btn"); 
+    addBtn.classList.add("add-btn");
 
     cancelBtn.classList.add("cancel-btn");
 
@@ -24,11 +40,12 @@ document.getElementById("add-project-btn").addEventListener("click", function(){
 
     inputContainer.classList.add("project-input-field-container");
 
-    inputContainer.appendChild(inputField); 
+    inputContainer.appendChild(inputField);
 
-    btnContainer.appendChild(addBtn); 
+    btnContainer.appendChild(addBtn);
 
     btnContainer.appendChild(cancelBtn);
 
     inputContainer.appendChild(btnContainer);
+
 }) 
