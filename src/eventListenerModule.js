4501494a414538f2c578f662cab145a7ac2ae867
alecnissen <alecnissen@yahoo.com>
2023-03-18 21:58:45 -0400

    added logic so that when user clicks add btn it will generate the btn back in the console, now working on grabbing the input value or the name of the project and appending it to the DOM

--- src/eventListenerModule.js
+++ src/eventListenerModule.js
@@ -11,17 +11,23 @@ import { addProjectDOMStuff } from "./domLogic";
 const addProjectButton = document.getElementById("add-project-btn"); 
 
   addProjectButton.addEventListener("click", function(){
-    // call the DOM Methods, associated with that button, 
-    // write a simple console log first to test to see if it's working 
-    
-   // call the DOM methods
 
    console.log("this is working, I am referencing my event module");
 
    console.log("alec");
 
-   addProjectDOMStuff();
+   addProjectDOMStuff(); 
 
+   [...document.querySelectorAll(".add-btn")] .at(-1).addEventListener("click", (e) => {
+    let ele = document.createElement('p'); 
+    ele.textContent = 'yooyyo';
+    console.log(ele);
+    console.log("Hey, I am inside a clicker!", e.target);
+   });
+
+  //  .at(-1).addEventListener("click", (e) => {
+  //   console.log("Hey, I am inside a clicker!", e.target);
+  //  });
 }) 
 
 // grab add btn element, add a listener to it 
@@ -31,16 +37,16 @@ const addProjectButton = document.getElementById("add-project-btn");
 
 
 
-const addProjectToDOMBtn = document.querySelectorAll(".add-btn");
+// const addProjectToDOMBtn = document.querySelectorAll(".add-btn");
 
-const newArr = Array.from(addProjectToDOMBtn);
+// const newArr = Array.from(addProjectToDOMBtn);
 
-newArr.forEach(button => {
-  console.log(button);
-  button.addEventListener("click", function(e){
-    console.log("Hey, inside a clicker!");
-  });
-})
+// newArr.forEach(button => {
+//   console.log(button);
+//   button.addEventListener("click", function(e){
+//     console.log("Hey, inside a clicker!");
+//   });
+// })
 
 
 // document.querySelectorAll(".add-btn").forEach( button => {
