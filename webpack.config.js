bbda9dfec4f117d043aa752badd273a431eb705c
alecnissen <alecnissen@yahoo.com>
2023-05-04 19:23:36 -0400

    testing to see if last two icons in delete project modal appear

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,17 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.html$/i,
+        use: "html-loader"
+    },
+    {
+        test: /\.(png|jpg)$/i,
+        type: "asset/resource",
+        generator: {
+            filename: "images/[name]-[hash][ext]"
+        }
+    }
     ],
   },
 };
